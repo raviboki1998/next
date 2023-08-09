5cd675b5bf6e02d781d2e03461cb1754279bbaa3
Ravi <raviboki1998@gmail.com>
2023-08-09 16:39:20 +0530

    final

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -12,9 +12,7 @@ const Hero = () => (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
-          </Link>
+          <Link href="https://github.com/">GitHub</Link>
         </li>
         <li>
           <Link href="/">Sign in</Link>
